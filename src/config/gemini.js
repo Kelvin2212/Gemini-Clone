8248bba62bfd486986f135826779995014f17083
Kelvin_Samuel <kelvinbalkes@gmail.com>
2024-07-14 20:36:58 +0700

    last commit

---
+++ src/config/gemini.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+// ESM Syntax
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyB8BuMtLEVXDhx1soSPrfhaulLEzmSi_go";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
